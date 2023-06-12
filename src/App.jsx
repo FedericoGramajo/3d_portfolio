c396d65800058c7a9f4342dec5aff24137ae3f32
FedericoGramajo <54726454+FedericoGramajo@users.noreply.github.com>
2023-06-11 21:32:04 -0300

    section contact and stars

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,15 @@ const App = () => {
           <Navbar />
           <Hero />
         </div>
-        <About/>
-        <Experience/>
-        <Tech/>
-        <Works/>
-        <Feedbacks/>
-        <div className="relative z-0"></div>
-        <Contact/>
-        <StarsCanvas/>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
       </div>
     </BrowserRouter>
   )
